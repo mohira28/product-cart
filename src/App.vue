87c456b3b359353bedbed0a77102f38632de648f
Mohira <mokhira.abdukarimova@bk.ru>
2022-03-31 18:31:32 +0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,71 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <header class="top-bar spread">
+        <nav class="top-bar-nav">
+          <router-link to="/" class="top-bar-link">
+            <i class="icofont-spoon-and-fork"></i>
+            <span>Home</span>
+          </router-link>
+          <router-link to="/products" class="top-bar-link">
+            <span>Products</span>
+          </router-link>
+          <router-link to="/past-orders" class="top-bar-link">
+            <span>Past Orders</span>
+          </router-link>
+        </nav>
+        <div @click="toggleSideBar" class="top-bar-cart-link">
+          <i class="icofont-cart-alt icofont-1x"></i>
+          <span>Cart ({{ totalQuantity }})</span>
+</div>
+      </header>
+  <router-view :inventory="inventory" :addToCart="addToCart"/>
+
+<SideBar
+  v-if="showSideBar"
+  :toggle="toggleSideBar"
+  :cart="cart"
+  :inventory="inventory"
+  :remove="removeItem"/>
+
 </template>
 
+<script>
+
+import food from './food.json'
+import SideBar from '@/components/SideBar.vue'
+export default {
+  components: {
+    SideBar
+  },
+  data () {
+    return {
+      showSideBar: false,
+      inventory: food,
+      cart: {}
+    }
+  },
+  computed: {
+    totalQuantity () {
+      return Object.values(this.cart).reduce((acc, curr) => {
+        return acc + curr
+      }, 0)
+    }
+  },
+  methods: {
+    addToCart (name, quantity) {
+      if (!this.cart[name]) this.cart[name] = 0
+      this.cart[name] += quantity
+    },
+    toggleSideBar () {
+      this.showSideBar = !this.showSideBar
+    },
+    removeItem (name) {
+      delete this.cart[name]
+    }
+  }
+}
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+*{
+  margin: 0;
 }
 </style>
